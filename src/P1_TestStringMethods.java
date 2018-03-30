5535c98a475111e0da163df9c68cb2a3a553fe76
sniperunit234 <37313753+sniperunit234@users.noreply.github.com>
2018-03-30 08:08:26 -0400

    issues
    i struggled with this alot

--- src/P1_TestStringMethods.java
+++ src/P1_TestStringMethods.java
@@ -7,19 +7,19 @@
 /*
  Your expression predictions:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. Frodo Baggins
+ 2. a
+ 3. Gandalf the Gray
+ 4.Frodo
  
- 5.
- 6.
- 7.
- 8.
+ 5. baggins
+ 6.Frodo
+ 7. Frodao Baggins
+ 8. Frodo Baggins
  
- 9.
- 10.
- 11.
+ 9. Gandalf the
+ 10. Gandalf the white
+ 11. Frodo
  
   
  */
@@ -27,24 +27,39 @@
 /*
  Were you correct? Explain if you were not (what did you learn?)
  
- 1.
- 2.
+ 1.  
+ 2. 
  3. 
  4.
  
- 5.
+ 5. 
  6.
- 7.
- 8.
+ 7. 
+ 8. 
  
- 9.
- 10.
- 11.
+ 9. 
+ 10. 
+ 11. 
  
  */
 public class P1_TestStringMethods {
 	public static void main(String[] args) {
+		String str1 = "Frodo Baggins";
+		String str2 = "Gandalf the GRAY";
 		System.out.println("REPLACE WITH YOUR EXPRESSION");
+		str1.length();
+		str1.charAt(7);
+		str2.charAt(0);
+		str1.indexOf("o");
+
+		str2.toUpperCase();
+		str1.toLowerCase();
+		str1.substring(4);
+		str2.substring(3, 14);
+		
+		str2.replace( "a", "oo");
+		str2.replace("gray", "white");
+		"str1".replace("r",  "range");
 	}
 
 }
--- src/P2_BananaStand.java
+++ src/P2_BananaStand.java
@@ -7,7 +7,15 @@
 
 public class P2_BananaStand {
 	public static void main(String[] args) {
-		String quote = "There's always money in the banana stand.";
+		String quote = "BANANA.";
+		System.out.println(quote);
+		
+	}
+
+	private static void lasthalf(String string) {
+		String lasthalf = "There's always money in the banana stand.";
+		System.out.println("lasthalf");
+			
 		
 	}
 }
